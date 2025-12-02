eb1ccab4e401efe005fb137f88940c49b5b13e2e
sleepyqwq <2436760359@qq.com>
2025-12-02 20:49:52 +0800

    init backend spring-boot project

---
+++ src/main/java/com/tq/JdOnlineMallApplication.java
@@ -0,0 +1,13 @@
+package com.tq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdOnlineMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdOnlineMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tq/JdOnlineMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdOnlineMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
